6a9e0d9d673db5cac87c27fd80593f8b63302d35
poncesaul <poncsaul012@gmail.com>
2017-05-29 16:14:53 -0600

    Add files via upload

---
+++ JerseySpring/src/com/example/spring/transaction/TransactionBO.java
@@ -0,0 +1,7 @@
+package com.example.spring.transaction;
+
+public interface TransactionBO {
+	
+	String save();
+	
+}
---
+++ JerseySpring/src/com/example/spring/transaction/impl/TransactionBOImpl.java
@@ -0,0 +1,11 @@
+package com.example.spring.transaction.impl;
+
+import com.example.spring.transaction.TransactionBO;
+
+public class TransactionBOImpl implements TransactionBO{
+
+	public String save() {
+		return "Jersey + Spring example";
+	}
+
+}
